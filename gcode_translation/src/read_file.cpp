6e56d099509350574587371a96807af37a361275
TIAO-JI-FU <t106360212@ntut.org.tw>
2019-09-24 15:26:22 +0800

    fix_bug_trjectory

--- gcode_translation/src/read_file.cpp
+++ gcode_translation/src/read_file.cpp
@@ -1,13 +1,8 @@
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 
-#include <moveit_msgs/DisplayRobotState.h>
-#include <moveit_msgs/DisplayTrajectory.h>
-
 #include <moveit_msgs/AttachedCollisionObject.h>
 #include <moveit_msgs/CollisionObject.h>
-
-#include <moveit_visual_tools/moveit_visual_tools.h>
 
 #include <trac_ik/trac_ik.hpp>
 #include <ros/ros.h>
@@ -21,11 +16,11 @@
 ros::WallTime start_, end_;
 int main(int argc, char **argv)
 {
+  start_ = ros::WallTime::now();
   ros::init(argc, argv, "move_group_1");
   ros::NodeHandle node_handle("~");
   ros::AsyncSpinner spinner(1);
   spinner.start();
-
   //----------------------------
   //Setup
   //----------------------------
@@ -43,18 +38,6 @@ int main(int argc, char **argv)
   //  move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
 
   geometry_msgs::PoseStamped current_pose = move_group.getCurrentPose();
-
-  // We can print the name of the reference frame for this robot.
-  // also printing the current position and orientation of the robot.
-  ros::Publisher pose_pub = node_handle.advertise<geometry_msgs::PoseStamped>("robot_pose", 10);
-  ros::Publisher pose_joint_pub = node_handle.advertise<sensor_msgs::JointState>("pose_joint", 10);
-  ROS_INFO_NAMED("moveo", "x position: %f", current_pose.pose.position.x);
-  ROS_INFO_NAMED("moveo", "y position: %f", current_pose.pose.position.y);
-  ROS_INFO_NAMED("moveo", "z position: %f", current_pose.pose.position.z);
-  ROS_INFO_NAMED("moveo", "x orientation: %f", current_pose.pose.orientation.x);
-  ROS_INFO_NAMED("moveo", "y orientation: %f", current_pose.pose.orientation.y);
-  ROS_INFO_NAMED("moveo", "z orientation: %f", current_pose.pose.orientation.z);
-  ROS_INFO_NAMED("moveo", "w orientation: %f", current_pose.pose.orientation.w);
 
   std::string chain_start, chain_end, urdf_param;
   double timeout;
@@ -74,7 +57,8 @@ int main(int argc, char **argv)
   node_handle.param("eps", eps, 1e-5);
   ROS_INFO_STREAM("eps :" << eps);
   TRAC_IK::TRAC_IK tracik_solver(chain_start, chain_end, urdf_param, timeout, eps, TRAC_IK::Distance);
-  TRAC_IK::TRAC_IK tracik_solver2(chain_start, chain_end, urdf_param, timeout*20, eps, TRAC_IK::Distance);
+  TRAC_IK::TRAC_IK tracik_solver2(chain_start, chain_end, urdf_param, timeout*10, eps, TRAC_IK::Distance);
+
   KDL::Chain chain;
   KDL::JntArray ll, ul; //lower joint limits, upper joint limits
 
@@ -93,35 +77,11 @@ int main(int argc, char **argv)
     ROS_ERROR("There were no valid KDL joint limits found");
     return -1;
   }
-
   assert(chain.getNrOfJoints() == ll.data.size());
   assert(chain.getNrOfJoints() == ul.data.size());
   ROS_INFO("Using %d joints", chain.getNrOfJoints());
   // Create Nominal chain configuration midway between all joint limits
   KDL::JntArray nominal(chain.getNrOfJoints());
-
-  // Visualization
-  // ^^^^^^^^^^^^^
-  //
-  // The package MoveItVisualTools provides many capabilties for visualizing objects, robots,
-  // and trajectories in Rviz as well as debugging tools such as step-by-step introspection of a script
-  namespace rvt = rviz_visual_tools;
-  moveit_visual_tools::MoveItVisualTools visual_tools("base_link");
-  visual_tools.deleteAllMarkers();
-
-  // Remote control is an introspection tool that allows users to step through a high level script
-  // via buttons and keyboard shortcuts in Rviz
-  visual_tools.loadRemoteControl();
-
-  // Rviz provides many types of markers, in this demo we will use text, cylinders, and spheres
-  Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
-  text_pose.translation().z() = 1.0; // above head of PR2
-  visual_tools.publishText(text_pose, "Moveo Demo", rvt::WHITE, rvt::XLARGE);
-
-  // Batch publishing is used to reduce the number of messages being sent to Rviz for large visualizations
-  visual_tools.trigger();
-
-
   //-----------------------------
   //Getting Basic Information
   //-----------------------------
@@ -132,18 +92,8 @@ int main(int argc, char **argv)
   // We can also print the name of the end-effector link for this group.
   ROS_INFO_NAMED("moveo", "End effector link: %s", move_group.getEndEffectorLink().c_str());
 
-  //-----------------------------
-  //Planning to a Pose Goal
-  //-----------------------------
-
-  //Plan a motion for this group to a desired pose for end-effector
-  // hardcode desired position here before running node in a separate terminal
-
   KDL::JntArray result;
   KDL::Vector end_effector_target_vol;
-  //node_handle.param("end_effector_target_vol_x", end_effector_target_vol.data[0], 0.0);
-  //node_handle.param("end_effector_target_vol_y", end_effector_target_vol.data[1], 0.3);
-  //node_handle.param("end_effector_target_vol_z", end_effector_target_vol.data[2], 0.4);
 
   KDL::Rotation end_effector_target_rot;
   double end_effector_target_R, end_effector_target_P,end_effector_target_Y;
@@ -151,10 +101,7 @@ int main(int argc, char **argv)
   node_handle.param("end_effector_target_rot_P", end_effector_target_P, 0.0);
   node_handle.param("end_effector_target_rot_Y", end_effector_target_Y, 0.0);
   end_effector_target_rot  = KDL::Rotation::RPY(end_effector_target_R, end_effector_target_P, end_effector_target_Y);
-  //node_handle.param("end_effector_target_rot", end_effector_rot,(0, 0.3, 0.4));
-  // Now, we call the planner to compute the plan and visualize it.
-  // Note that we are just planning, not asking move_group
-  // to actually move the robot.
+
   int rc;
   std::vector<KDL::JntArray> JointList;
   geometry_msgs::Pose target_pose1;
@@ -164,26 +111,26 @@ int main(int argc, char **argv)
                 start_state.getJointModelGroup(move_group.getName());
   KDL::Vector target_bounds_rot(0, 0, 2* M_PI), target_bounds_vel(0,0,0);
   const KDL::Twist target_bounds(target_bounds_vel, target_bounds_rot);
-  std::ifstream inputFile("/home/arthur/git_ws/moveo_moveit_ws/src/moveo_moveit_move_group/src/15mm test.gcode");
+  std::ifstream inputFile("/home/arthur/git_ws/moveo_moveit_ws/src/gcode_translation/src/15mm test.gcode");
+  std::ofstream outfile ("/home/arthur/git_ws/moveo_moveit_ws/src/gcode_translation/src/15mm test_new.gcode");
   std::string line;
+  std::string file_line;
   bool check = 0;
   int second_execution = 0;
   while(ros::ok()){
     while(getline(inputFile, line)){
       std::istringstream get_line(line);
-      std::string file_line;
       file_line = get_line.str();
       if(file_line.find('G') < 1){
         if(file_line.find('0') < 2){
           check = 1;
         }
       }
-      size_t colon_pos_non = file_line.find(';');
-      if(colon_pos_non < 1){
-        size_t colon_pos_G = file_line.find('G');
-        if(colon_pos_G < 9){
-          size_t colon_pos_c = file_line.find('c');
-          if(colon_pos_c < 10){
+      if(file_line.find(';') < 1){
+        if(file_line.find('G') < 9){
+          if(file_line.find('c') < 10){
+            outfile << file_line << std::endl;
+            outfile.close();
             end_ = ros::WallTime::now();
             double execution_time = (end_ - start_).toNSec() * 1e-9;
             ROS_INFO_STREAM("Exectution time (ms): " << execution_time);
@@ -192,17 +139,19 @@ int main(int argc, char **argv)
             return 0;
           }
         }
+        outfile << file_line << std::endl;
       }
       else if(check == 1){
-        if (file_line.find('G') < 1){
+        if(file_line.find('G') < 1){
           if(file_line.find('0') < 2 || file_line.find('1') < 2){
+            outfile << file_line[0] << file_line[1];
             size_t colon_pos_X = file_line.find('X');
             if(colon_pos_X < 100){
               end_effector_target_vol.data[0] = stod(file_line.substr(colon_pos_X+1))*1e-3;
             }
             size_t colon_pos_Y = file_line.find('Y');
             if(colon_pos_Y < 100){
-              end_effector_target_vol.data[1] = (stod(file_line.substr(colon_pos_Y+1))*1e-3)+0.2;
+              end_effector_target_vol.data[1] = (stod(file_line.substr(colon_pos_Y+1))*1e-3)+0.05;
             }
             size_t colon_pos_Z = file_line.find('Z');
             if(colon_pos_Z < 100){
@@ -214,57 +163,48 @@ int main(int argc, char **argv)
               rc = tracik_solver2.CartToJnt(nominal, end_effector_pose, result, target_bounds);
               second_execution++;
             }
-            // Create desired number of valid, random joint configurations
             ROS_INFO_STREAM("rc :" << rc);
-            //visual_tools.deleteAllMarkers();
             target_pose1.position.x    = end_effector_pose.p.x();
             target_pose1.position.y    = end_effector_pose.p.y();
             target_pose1.position.z    = end_effector_pose.p.z();
             end_effector_pose.M.GetQuaternion	(target_pose1.orientation.x, target_pose1.orientation.y, target_pose1.orientation.z, target_pose1.orientation.w);
-
-            if (rc >= 0){
-              target_joints = move_group.getCurrentJointValues();
+            if(rc >= 0){
               for(int i = 0; i < chain.getNrOfJoints(); i++){
-                target_joints.at(i) = result.data(i);
+                switch (i){
+                  case 0:
+                  outfile << " J" << int(result.data(i)*65600/(2*M_PI));
+                  break;
+                  case 1:
+                  outfile << " A" << int(result.data(i)*18000/(2*M_PI));
+                  break;
+                  case 2:
+                  outfile << " B" << int(result.data(i)*144000/(2*M_PI));
+                  break;
+                  case 3:
+                  outfile << " C" << int(result.data(i)*6560/(2*M_PI));
+                  break;
+                  case 4:
+                  outfile << " D" << int(result.data(i)*28800/(2*M_PI));
+                  break;
+                }
               }
-              move_group.setJointValueTarget(target_joints);
-              moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-              move_group.plan(my_plan);
-              move_group.setMaxVelocityScalingFactor(1);
-              move_group.setMaxAccelerationScalingFactor(1);
-              //visual_tools.publishAxisLabeled(target_pose1, "target_pose1");
-              visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
-              visual_tools.trigger();
-              move_group.move();
-              //start_state.setJointGroupPositions(joint_model_group, target_joints);
-              //start_state.setFromIK(joint_model_group, target_pose1);
-              //move_group.setStartState(start_state);
-              //move_group.move();
-              //ROS_INFO_STREAM("joint :" << target_joints);
-              //current_pose = move_group.getCurrentPose();
-              // We can print the name of the reference frame for this robot.
-              // also printing the current position and orientation of the robot.
-              //ROS_INFO_NAMED("moveo", "x position: %f", current_pose.pose.position.x);
-              //ROS_INFO_NAMED("moveo", "y position: %f", current_pose.pose.position.y);
-              //ROS_INFO_NAMED("moveo", "z position: %f", current_pose.pose.position.z);
-              //ROS_INFO_NAMED("moveo", "x orientation: %f", current_pose.pose.orientation.x);
-              //ROS_INFO_NAMED("moveo", "y orientation: %f", current_pose.pose.orientation.y);
-              //ROS_INFO_NAMED("moveo", "z orientation: %f", current_pose.pose.orientation.z);
-              //ROS_INFO_NAMED("moveo", "w orientation: %f", current_pose.pose.orientation.w);
-              //sensor_msgs::JointState moveo_joint_state;
-              //std::vector<std::string> name{"moveo_joint1", "moveo_joint2", "moveo_joint3", "moveo_joint4", "moveo_joint5"};
-              //std::vector<double> position = move_group.getCurrentJointValues();
-              //moveo_joint_state.name = name;
-              //moveo_joint_state.position = position;
-              //pose_joint_pub.publish(moveo_joint_state);
+              for(int j = 2;j < file_line.length(); j++){
+                outfile << file_line[j];
+              }
+              outfile << std::endl;
             }
             else{
-              ROS_INFO_STREAM("second_execution: " << second_execution);
               ros::shutdown();
               return 0;
             }
           }
         }
+        else{
+          outfile << file_line << std::endl;
+        }
+      }
+      else{
+        outfile << file_line << std::endl;
       }
     }
   }
