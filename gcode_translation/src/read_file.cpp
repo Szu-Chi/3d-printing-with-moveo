ab08b68174d283e0d08cc445bdf30edc35dbd505
Szu-Chi <t106360231@ntut.org.tw>
2019-09-25 23:38:32 +0800

    let code more legibility

--- gcode_translation/src/read_file.cpp
+++ gcode_translation/src/read_file.cpp
@@ -13,6 +13,30 @@
 #include <time.h>
 #include <math.h>
 
+bool check_trac_ik_valid(TRAC_IK::TRAC_IK &tracik_solver,KDL::Chain &chain, KDL::JntArray &ll, KDL::JntArray &ul){
+  bool valid = tracik_solver.getKDLChain(chain);
+  ROS_INFO_NAMED("moveo", "TRAC-IK setup");
+  if (!valid)
+  {
+    ROS_ERROR("There was no valid KDL chain found");
+    return false;
+  }
+  valid = tracik_solver.getKDLLimits(ll, ul);
+  if (!valid)
+  {
+    ROS_ERROR("There were no valid KDL joint limits found");
+    return false;
+  }
+  return true;
+}
+
+void motor_setep_convert(Eigen::VectorXd &data){
+  static const double joint_division[5] = {65600, 18000, 144000, 6560, 28800};
+  for(int i = 0; i < 5; i++){
+    data(i) = data(i) * joint_division[i]/(2*M_PI);
+  }
+ }
+
 ros::WallTime start_, end_;
 int main(int argc, char **argv)
 {
@@ -44,22 +68,9 @@ int main(int argc, char **argv)
 
   KDL::Chain chain;
   KDL::JntArray ll, ul; //lower joint limits, upper joint limits
+  if(!check_trac_ik_valid(tracik_solver, chain, ll, ul)) return -1;
+  if(!check_trac_ik_valid(tracik_solver2, chain, ll, ul)) return -1;
 
-  bool valid = tracik_solver.getKDLChain(chain);
-  ROS_INFO_NAMED("moveo", "TRAC-IK setup");
-  if (!valid)
-  {
-    ROS_ERROR("There was no valid KDL chain found");
-    return -1;
-  }
-
-  valid = tracik_solver.getKDLLimits(ll, ul);
-
-  if (!valid)
-  {
-    ROS_ERROR("There were no valid KDL joint limits found");
-    return -1;
-  }
   assert(chain.getNrOfJoints() == ll.data.size());
   assert(chain.getNrOfJoints() == ul.data.size());
   ROS_INFO("Using %d joints", chain.getNrOfJoints());
@@ -83,6 +94,7 @@ int main(int argc, char **argv)
   std::string gcode_in, gcode_out;
   node_handle.param("gcode_in", gcode_in, std::string("/gcode_in"));
   node_handle.param("gcode_out", gcode_out, std::string("/gcode_out")); 
+  
   std::ifstream input_file(gcode_in);
   if(!input_file.is_open())ROS_ERROR_STREAM("Can't open " <<gcode_in);
   
@@ -90,22 +102,20 @@ int main(int argc, char **argv)
   if(!output_file.is_open())ROS_ERROR_STREAM("Can't open " <<gcode_out);
   
   std::string line;
-  std::string file_line;
   bool check = 0;
   int second_execution = 0;
   while(ros::ok()){
-    while(getline(input_file, line)){
-      std::istringstream get_line(line);
-      file_line = get_line.str();
-      if(file_line.find('G') < 1){
-        if(file_line.find('0') < 2){
-          check = 1;
-        }
+    while(input_file){
+      std::getline(input_file, line);
+      if(!line.compare(0,2,"G0")){
+        check = 1;
+      }else{
+        if(!check)std::cout << line << std::endl;
       }
-      if(file_line.find(';') < 1){
-        if(file_line.find('G') < 9){
-          if(file_line.find('c') < 10){
-            output_file << file_line << std::endl;
+
+      if(!line.compare(0,1,";")){
+        if(!line.compare(";End of Gcode")){
+            output_file << line << std::endl;
             output_file.close();
             end_ = ros::WallTime::now();
             double execution_time = (end_ - start_).toNSec() * 1e-9;
@@ -113,74 +123,57 @@ int main(int argc, char **argv)
             ROS_INFO_STREAM("second_execution: " << second_execution);
             ros::shutdown();
             return 0;
-          }
         }
-        output_file << file_line << std::endl;
+        output_file << line << std::endl;
       }
       else if(check == 1){
-        if(file_line.find('G') < 1){
-          if(file_line.find('0') < 2 || file_line.find('1') < 2){
-            output_file << file_line[0] << file_line[1];
-            size_t colon_pos_X = file_line.find('X');
-            if(colon_pos_X < 100){
-              end_effector_target_vol.data[0] = stod(file_line.substr(colon_pos_X+1))*1e-3;
+        if(!line.compare(0,2,"G0") || !line.compare(0,2,"G1")){
+          output_file << line[0] << line[1];
+          size_t colon_pos_X = line.find('X');
+          if(colon_pos_X < 100){
+            end_effector_target_vol.data[0] = stod(line.substr(colon_pos_X+1))*1e-3;
+          }
+          size_t colon_pos_Y = line.find('Y');
+          if(colon_pos_Y < 100){
+            end_effector_target_vol.data[1] = (stod(line.substr(colon_pos_Y+1))*1e-3)+0.30;
+          }
+          size_t colon_pos_Z = line.find('Z');
+          if(colon_pos_Z < 100){
+            end_effector_target_vol.data[2] = stod(line.substr(colon_pos_Z+1))*1e-3;
+          }
+          KDL::Frame end_effector_pose(end_effector_target_rot, end_effector_target_vol);
+          rc = tracik_solver.CartToJnt(nominal, end_effector_pose, result, target_bounds);
+          if(rc < 0){
+            rc = tracik_solver2.CartToJnt(nominal, end_effector_pose, result, target_bounds);
+            second_execution++;
+          }
+          ROS_INFO_STREAM("rc :" << rc);
+          target_pose1.position.x    = end_effector_pose.p.x();
+          target_pose1.position.y    = end_effector_pose.p.y();
+          target_pose1.position.z    = end_effector_pose.p.z();
+          end_effector_pose.M.GetQuaternion	(target_pose1.orientation.x, target_pose1.orientation.y, target_pose1.orientation.z, target_pose1.orientation.w);
+          if(rc >= 0){
+            for(int i = 0; i < chain.getNrOfJoints(); i++){
+              static const char joint_code[5] = {'J', 'A', 'B', 'C', 'D'};
+              motor_setep_convert(result.data);
+              output_file << " " << joint_code[i] << result.data(i);
             }
-            size_t colon_pos_Y = file_line.find('Y');
-            if(colon_pos_Y < 100){
-              end_effector_target_vol.data[1] = (stod(file_line.substr(colon_pos_Y+1))*1e-3)+0.35;
+            for(int j = 2;j < line.length(); j++){
+              output_file << line[j];
             }
-            size_t colon_pos_Z = file_line.find('Z');
-            if(colon_pos_Z < 100){
-              end_effector_target_vol.data[2] = stod(file_line.substr(colon_pos_Z+1))*1e-3;
-            }
-            KDL::Frame end_effector_pose(end_effector_target_rot, end_effector_target_vol);
-            rc = tracik_solver.CartToJnt(nominal, end_effector_pose, result, target_bounds);
-            if(rc < 0){
-              rc = tracik_solver2.CartToJnt(nominal, end_effector_pose, result, target_bounds);
-              second_execution++;
-            }
-            ROS_INFO_STREAM("rc :" << rc);
-            target_pose1.position.x    = end_effector_pose.p.x();
-            target_pose1.position.y    = end_effector_pose.p.y();
-            target_pose1.position.z    = end_effector_pose.p.z();
-            end_effector_pose.M.GetQuaternion	(target_pose1.orientation.x, target_pose1.orientation.y, target_pose1.orientation.z, target_pose1.orientation.w);
-            if(rc >= 0){
-              for(int i = 0; i < chain.getNrOfJoints(); i++){
-                switch (i){
-                  case 0:
-                  output_file << " J" << int(result.data(i)*65600/(2*M_PI));
-                  break;
-                  case 1:
-                  output_file << " A" << int(result.data(i)*18000/(2*M_PI));
-                  break;
-                  case 2:
-                  output_file << " B" << int(result.data(i)*144000/(2*M_PI));
-                  break;
-                  case 3:
-                  output_file << " C" << int(result.data(i)*6560/(2*M_PI));
-                  break;
-                  case 4:
-                  output_file << " D" << int(result.data(i)*28800/(2*M_PI));
-                  break;
-                }
-              }
-              for(int j = 2;j < file_line.length(); j++){
-                output_file << file_line[j];
-              }
-              output_file << std::endl;
-            }
-            else{
-              ros::shutdown();
-              return 0;
-            }
+            output_file << std::endl;
+          }
+          else{
+            ros::shutdown();
+            return 0;
           }
         }
         else{
-          output_file << file_line << std::endl;
+          output_file << line << std::endl;
         }
       }
       else{
-        output_file << file_line << std::endl;
+        output_file << line << std::endl;
       }
     }
   }
