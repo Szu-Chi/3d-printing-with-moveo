8fc7462bf73ac35e3acee69388c56a7e0f13e9fc
TIAO-JI-FU <t106360212@ntut.org.tw>
2019-10-02 01:33:40 +0800

    straight line

--- gcode_translation/src/read_file.cpp
+++ gcode_translation/src/read_file.cpp
@@ -13,18 +13,41 @@
 #include <time.h>
 #include <math.h>
 
+bool check_trac_ik_valid(TRAC_IK::TRAC_IK &tracik_solver,KDL::Chain &chain, KDL::JntArray &ll, KDL::JntArray &ul){
+  bool valid = tracik_solver.getKDLChain(chain);
+  ROS_INFO_NAMED("moveo", "TRAC-IK setup");
+  if (!valid)
+  {
+    ROS_ERROR("There was no valid KDL chain found");
+    return false;
+  }
+  valid = tracik_solver.getKDLLimits(ll, ul);
+  if (!valid)
+  {
+    ROS_ERROR("There were no valid KDL joint limits found");
+    return false;
+  }
+  return true;
+}
+
+void motor_setep_convert(Eigen::VectorXd &data){
+  static const double joint_division[5] = {65600, 18000, 144000, 6560, 28800};
+  for(int i = 0; i < 5; i++){
+    data(i) = data(i) * joint_division[i]/(2*M_PI);
+  }
+ }
+
 ros::WallTime start_, end_;
 int main(int argc, char **argv)
 {
   start_ = ros::WallTime::now();
-  ros::init(argc, argv, "move_group_1");
+  ros::init(argc, argv, "gcode_translation");
   ros::NodeHandle node_handle("~");
   ros::AsyncSpinner spinner(1);
   spinner.start();
-  static const std::string PLANNING_GROUP = "arm";
-  moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
-  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-  geometry_msgs::PoseStamped current_pose = move_group.getCurrentPose();
+  //----------------------------
+  //Setup
+  //----------------------------
   std::string chain_start, chain_end, urdf_param;
   double timeout;
   node_handle.param("chain_start", chain_start, std::string(""));
@@ -36,149 +59,175 @@ int main(int argc, char **argv)
   }
   node_handle.param("timeout", timeout, 0.005);
   node_handle.param("urdf_param", urdf_param, std::string("/robot_description"));
+
   double eps = 1e-5;
   node_handle.param("eps", eps, 1e-5);
   ROS_INFO_STREAM("eps :" << eps);
   TRAC_IK::TRAC_IK tracik_solver(chain_start, chain_end, urdf_param, timeout, eps, TRAC_IK::Distance);
-  TRAC_IK::TRAC_IK tracik_solver2(chain_start, chain_end, urdf_param, timeout*10, eps, TRAC_IK::Distance);
+  TRAC_IK::TRAC_IK tracik_solver2(chain_start, chain_end, urdf_param, timeout*20, eps, TRAC_IK::Distance);
+
   KDL::Chain chain;
-  KDL::JntArray ll, ul;
-  bool valid = tracik_solver.getKDLChain(chain);
-  ROS_INFO_NAMED("moveo", "TRAC-IK setup");
-  if (!valid)
-  {
-    ROS_ERROR("There was no valid KDL chain found");
-    return -1;
-  }
+  KDL::JntArray ll, ul; //lower joint limits, upper joint limits
+  if(!check_trac_ik_valid(tracik_solver, chain, ll, ul)) return -1;
+  if(!check_trac_ik_valid(tracik_solver2, chain, ll, ul)) return -1;
 
-  valid = tracik_solver.getKDLLimits(ll, ul);
-
-  if (!valid)
-  {
-    ROS_ERROR("There were no valid KDL joint limits found");
-    return -1;
-  }
   assert(chain.getNrOfJoints() == ll.data.size());
   assert(chain.getNrOfJoints() == ul.data.size());
   ROS_INFO("Using %d joints", chain.getNrOfJoints());
+  // Create Nominal chain configuration midway between all joint limits
   KDL::JntArray nominal(chain.getNrOfJoints());
-  ROS_INFO_NAMED("moveo", "Reference frame: %s", move_group.getPlanningFrame().c_str());
-  ROS_INFO_NAMED("moveo", "End effector link: %s", move_group.getEndEffectorLink().c_str());
+  //-----------------------------
+  //Getting Basic Information
+  //-----------------------------
 
   KDL::JntArray result;
   KDL::Vector end_effector_target_vol;
-
+  KDL::Vector pre_end_effector_target_vol;
   KDL::Rotation end_effector_target_rot;
-  double end_effector_target_R, end_effector_target_P,end_effector_target_Y;
-  node_handle.param("end_effector_target_rot_R", end_effector_target_R, 0.0);
-  node_handle.param("end_effector_target_rot_P", end_effector_target_P, 0.0);
-  node_handle.param("end_effector_target_rot_Y", end_effector_target_Y, 0.0);
-  end_effector_target_rot  = KDL::Rotation::RPY(end_effector_target_R, end_effector_target_P, end_effector_target_Y);
 
   int rc;
   std::vector<KDL::JntArray> JointList;
   geometry_msgs::Pose target_pose1;
   std::vector<double> target_joints;
-  robot_state::RobotState start_state(*move_group.getCurrentState());
-  const robot_state::JointModelGroup *joint_model_group =
-                start_state.getJointModelGroup(move_group.getName());
   KDL::Vector target_bounds_rot(0, 0, 2* M_PI), target_bounds_vel(0,0,0);
   const KDL::Twist target_bounds(target_bounds_vel, target_bounds_rot);
-  std::ifstream inputFile("/home/arthur/git_ws/moveo_moveit_ws/src/gcode_translation/src/75mm test.gcode");
-  std::ofstream outfile ("/home/arthur/git_ws/moveo_moveit_ws/src/gcode_translation/src/75mm test_new.gcode");
+
+  std::string gcode_in, gcode_out;
+  node_handle.param("gcode_in", gcode_in, std::string("/gcode_in"));
+  node_handle.param("gcode_out", gcode_out, std::string("/gcode_out"));
+
+  std::ifstream input_file(gcode_in);
+  if(!input_file.is_open())ROS_ERROR_STREAM("Can't open " <<gcode_in);
+
+  std::ofstream output_file(gcode_out);
+  if(!output_file.is_open())ROS_ERROR_STREAM("Can't open " <<gcode_out);
+
   std::string line;
-  std::string file_line;
   bool check = 0;
+  bool check_distance = 0;
   int second_execution = 0;
-  while(ros::ok()){
-    while(getline(inputFile, line)){
-      std::istringstream get_line(line);
-      file_line = get_line.str();
-      if(file_line.find('G') < 1){
-        if(file_line.find('0') < 2){
-          check = 1;
+  double E_out = 0;
+  double pre_E_out = 0;
+  while(input_file){
+    std::getline(input_file, line);
+    if(!line.compare(0,2,"G0")){
+      check = 1;
+    }
+    else{
+      if(!check)std::cout << line << std::endl;
+    }
+    if(!line.compare(0,1,";")){
+      output_file << line << std::endl;
+    }
+    else if(check == 1){
+      if(!line.compare(0,2,"G0") || !line.compare(0,2,"G1")){
+        output_file << line[0] << line[1];
+        size_t colon_pos_X = line.find('X');
+        if(colon_pos_X < 100){
+          end_effector_target_vol.data[0] = stod(line.substr(colon_pos_X+1))*1e-3;
         }
-      }
-      if(file_line.find(';') < 1){
-        if(file_line.find('G') < 9){
-          if(file_line.find('c') < 10){
-            outfile << file_line << std::endl;
-            outfile.close();
-            end_ = ros::WallTime::now();
-            double execution_time = (end_ - start_).toNSec() * 1e-9;
-            ROS_INFO_STREAM("Exectution time (ms): " << execution_time);
-            ROS_INFO_STREAM("second_execution: " << second_execution);
-            ros::shutdown();
-            return 0;
+        size_t colon_pos_Y = line.find('Y');
+        if(colon_pos_Y < 100){
+          end_effector_target_vol.data[1] = (stod(line.substr(colon_pos_Y+1))*1e-3)+0.30;
+        }
+        size_t colon_pos_Z = line.find('Z');
+        if(colon_pos_Z < 100){
+          end_effector_target_vol.data[2] = stod(line.substr(colon_pos_Z+1))*1e-3;
+        }
+        size_t colon_pos_E = line.find('E');
+        if(colon_pos_E < 100){
+           if(stod(line.substr(colon_pos_E+1)) != 0){
+              E_out = stod(line.substr(colon_pos_E+1));
+           }
+        }
+        if(check_distance == 1){
+          double X = (end_effector_target_vol.data[0] - pre_end_effector_target_vol.data[0]);
+          double Y = (end_effector_target_vol.data[1] - pre_end_effector_target_vol.data[1]);
+          double E = E_out - pre_E_out;
+          int cut_part = 1;
+          while(sqrt(pow(X/cut_part,2)+pow(Y/cut_part,2)) > 0.015){
+            cut_part++;
           }
-        }
-        outfile << file_line << std::endl;
-      }
-      else if(check == 1){
-        if(file_line.find('G') < 1){
-          if(file_line.find('0') < 2 || file_line.find('1') < 2){
-            outfile << file_line[0] << file_line[1];
-            size_t colon_pos_X = file_line.find('X');
-            if(colon_pos_X < 100){
-              end_effector_target_vol.data[0] = stod(file_line.substr(colon_pos_X+1))*1e-3;
-            }
-            size_t colon_pos_Y = file_line.find('Y');
-            if(colon_pos_Y < 100){
-              end_effector_target_vol.data[1] = (stod(file_line.substr(colon_pos_Y+1))*1e-3)+0.30;
-            }
-            size_t colon_pos_Z = file_line.find('Z');
-            if(colon_pos_Z < 100){
-              end_effector_target_vol.data[2] = stod(file_line.substr(colon_pos_Z+1))*1e-3;
-            }
-            KDL::Frame end_effector_pose(end_effector_target_rot, end_effector_target_vol);
-            rc = tracik_solver.CartToJnt(nominal, end_effector_pose, result, target_bounds);
+          for(int i = 1; i < cut_part; i++){
+            pre_end_effector_target_vol.data[0] += X/cut_part;
+            pre_end_effector_target_vol.data[1] += Y/cut_part;
+            pre_E_out += E/cut_part;
+            KDL::Frame pre_end_effector_pose(end_effector_target_rot, pre_end_effector_target_vol);
+            ROS_INFO_STREAM("happend");
+            rc = tracik_solver.CartToJnt(nominal, pre_end_effector_pose, result, target_bounds);
             if(rc < 0){
-              rc = tracik_solver2.CartToJnt(nominal, end_effector_pose, result, target_bounds);
+              rc = tracik_solver2.CartToJnt(nominal, pre_end_effector_pose, result, target_bounds);
               second_execution++;
             }
             ROS_INFO_STREAM("rc :" << rc);
-            target_pose1.position.x = end_effector_pose.p.x();
-            target_pose1.position.y = end_effector_pose.p.y();
-            target_pose1.position.z = end_effector_pose.p.z();
-            end_effector_pose.M.GetQuaternion	(target_pose1.orientation.x, target_pose1.orientation.y, target_pose1.orientation.z, target_pose1.orientation.w);
+            motor_setep_convert(result.data);
             if(rc >= 0){
               for(int i = 0; i < chain.getNrOfJoints(); i++){
-                switch (i){
-                  case 0:
-                  outfile << " J" << int(result.data(i)*65600/(2*M_PI));
-                  break;
-                  case 1:
-                  outfile << " A" << int(result.data(i)*18000/(2*M_PI));
-                  break;
-                  case 2:
-                  outfile << " B" << int(result.data(i)*144000/(2*M_PI));
-                  break;
-                  case 3:
-                  outfile << " C" << int(result.data(i)*6560/(2*M_PI));
-                  break;
-                  case 4:
-                  outfile << " D" << int(result.data(i)*28800/(2*M_PI));
-                  break;
+                static const char joint_code[5] = {'J', 'A', 'B', 'C', 'D'};
+                output_file << " " << joint_code[i] << int(result.data(i));
+              }
+              output_file << " X" << pre_end_effector_target_vol.data[0]*1e3;
+              output_file << " Y" << (pre_end_effector_target_vol.data[1]-0.3)*1e3;
+              if(colon_pos_E < 100){
+                if(stod(line.substr(colon_pos_E+1)) != 0){
+                  output_file << " E" << pre_E_out;
                 }
               }
-              for(int j = 2;j < file_line.length(); j++){
-                outfile << file_line[j];
-              }
-              outfile << std::endl;
+              output_file << std::endl;
+              output_file << line[0] << line[1];
             }
             else{
+              ROS_INFO_STREAM("second_execution: " << second_execution);
               ros::shutdown();
               return 0;
             }
           }
         }
-        else{
-          outfile << file_line << std::endl;
+        KDL::Frame end_effector_pose(end_effector_target_rot, end_effector_target_vol);
+        rc = tracik_solver.CartToJnt(nominal, end_effector_pose, result, target_bounds);
+        if(rc < 0){
+          rc = tracik_solver2.CartToJnt(nominal, end_effector_pose, result, target_bounds);
+          second_execution++;
         }
+        ROS_INFO_STREAM("rc :" << rc);
+        target_pose1.position.x    = end_effector_pose.p.x();
+        target_pose1.position.y    = end_effector_pose.p.y();
+        target_pose1.position.z    = end_effector_pose.p.z();
+        end_effector_pose.M.GetQuaternion	(target_pose1.orientation.x, target_pose1.orientation.y, target_pose1.orientation.z, target_pose1.orientation.w);
+        motor_setep_convert(result.data);
+        if(rc >= 0){
+          for(int i = 0; i < chain.getNrOfJoints(); i++){
+            static const char joint_code[5] = {'J', 'A', 'B', 'C', 'D'};
+            output_file << " " << joint_code[i] << int(result.data(i));
+          }
+          for(int j = 2;j < line.length(); j++){
+            output_file << line[j];
+          }
+          output_file << std::endl;
+        }
+        else{
+          ROS_INFO_STREAM("second_execution: " << second_execution);
+          ros::shutdown();
+          return 0;
+        }
+        pre_end_effector_target_vol.data[0] = end_effector_target_vol.data[0];
+        pre_end_effector_target_vol.data[1] = end_effector_target_vol.data[1];
+        pre_end_effector_target_vol.data[2] = end_effector_target_vol.data[2];
+        pre_E_out = E_out;
       }
       else{
-        outfile << file_line << std::endl;
+        output_file << line << std::endl;
       }
+      check_distance = 1;
+    }
+    else{
+      output_file << line << std::endl;
     }
   }
+  end_ = ros::WallTime::now();
+  double execution_time = (end_ - start_).toNSec() * 1e-9;
+  ROS_INFO_STREAM("Exectution time (ms): " << execution_time);
+  ROS_INFO_STREAM("second_execution: " << second_execution);
+  ros::shutdown();
+  return 0;
 }
--- virtual_shift/src/virtual_shift.cpp
+++ virtual_shift/src/virtual_shift.cpp
@@ -25,101 +25,88 @@ int main(int argc, char **argv)
   target_joints = move_group.getCurrentJointValues();
   moveit::core::RobotStatePtr start_state(move_group.getCurrentState());
   const robot_state::JointModelGroup *joint_model_group = start_state->getJointModelGroup(move_group.getName());
-  //start_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
   bool check = 0;
-  bool A = 0;
-  bool K = 0;
-  std::ifstream inputFile("/home/arthur/git_ws/moveo_moveit_ws/src/gcode_translation/src/75mm test_new.gcode");
-  std::string line;
+  std::string gcode_in;
+  node_handle.param("gcode_in", gcode_in, std::string("/gcode_in"));
+  std::ifstream input_file(gcode_in);
+  if(!input_file.is_open())ROS_ERROR_STREAM("Can't open " <<gcode_in);
   moveit_msgs::RobotTrajectory trajectory;
-  while(ros::ok()){
-    while(getline(inputFile, line)){
-      std::istringstream get_line(line);
-      std::string file_line;
-      file_line = get_line.str();
-      if(check == 0){
-        if(file_line.find('G') < 1){
-          if(file_line.find('0') < 2){
-            check = 1;
+
+  std::string line;
+  int first = 0;
+  while(input_file){
+    std::getline(input_file, line);
+    if(!line.compare(0,2,"G0")){
+      check = 1;
+    }
+
+    if(!line.compare(0,1,";")){
+    }
+    else if(check == 1){
+      if(!line.compare(0,2,"G0") || !line.compare(0,2,"G1")){
+        size_t colon_pos_J = line.find('J');
+        if(colon_pos_J < 100){
+          target_joints.at(0) = double(stod(line.substr(colon_pos_J+1))*(2*M_PI)/65600);
+        }
+        size_t colon_pos_A = line.find('A');
+        if(colon_pos_A < 100){
+          target_joints.at(1) = double(stod(line.substr(colon_pos_A+1))*(2*M_PI)/18000);
+        }
+        size_t colon_pos_B = line.find('B');
+        if(colon_pos_B < 100){
+          target_joints.at(2) = double(stod(line.substr(colon_pos_B+1))*(2*M_PI)/144000);
+        }
+        size_t colon_pos_C = line.find('C');
+        if(colon_pos_C < 100){
+          target_joints.at(3) = double(stod(line.substr(colon_pos_C+1))*(2*M_PI)/6560);
+        }
+        size_t colon_pos_D = line.find('D');
+        if(colon_pos_D < 100){
+          target_joints.at(4) = double(stod(line.substr(colon_pos_D+1))*(2*M_PI)/28800);
+        }
+        move_group.setJointValueTarget(target_joints);
+        if(first++ > 2){
+          moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+          move_group.plan(my_plan);
+          trajectory.joint_trajectory.joint_names = my_plan.trajectory_.joint_trajectory.joint_names;
+          joint_model_group = start_state->getJointModelGroup(move_group.getName());
+          start_state->setJointGroupPositions(joint_model_group, target_joints);
+          move_group.setStartState(*start_state);
+          for(int j = 0; j < my_plan.trajectory_.joint_trajectory.points.size(); j++){
+            trajectory.joint_trajectory.points.push_back(my_plan.trajectory_.joint_trajectory.points[j]);
+          }
+          size_t colon_pos_E = line.find('E');
+          if(colon_pos_E < 100){
+            if(stod(line.substr(colon_pos_E+1)) > 0){
+              visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
+              visual_tools.trigger();
+            }
           }
         }
-      }
-      if(check == 1){
-        if(file_line.find(';') < 1){
-          if(file_line.find('G') < 9){
-            if(file_line.find('c') < 10){
-              K=1;
-            }
-          }
-        }
-        if (file_line.find('G') < 1){
-          if(file_line.find('0') < 2 || file_line.find('1') < 2){
-            size_t colon_pos_J = file_line.find('J');
-            if(colon_pos_J < 100){
-              target_joints.at(0) = double(stod(file_line.substr(colon_pos_J+1))*(2*M_PI)/65600);
-            }
-            size_t colon_pos_A = file_line.find('A');
-            if(colon_pos_A < 100){
-              target_joints.at(1) = double(stod(file_line.substr(colon_pos_A+1))*(2*M_PI)/18000);
-            }
-            size_t colon_pos_B = file_line.find('B');
-            if(colon_pos_B < 100){
-              target_joints.at(2) = double(stod(file_line.substr(colon_pos_B+1))*(2*M_PI)/144000);
-            }
-            size_t colon_pos_C = file_line.find('C');
-            if(colon_pos_C < 100){
-              target_joints.at(3) = double(stod(file_line.substr(colon_pos_C+1))*(2*M_PI)/6560);
-            }
-            size_t colon_pos_D = file_line.find('D');
-            if(colon_pos_D < 100){
-              target_joints.at(4) = double(stod(file_line.substr(colon_pos_D+1))*(2*M_PI)/28800);
-            }
-            move_group.setJointValueTarget(target_joints);
-            moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-            moveit::planning_interface::MoveItErrorCode success = move_group.plan(my_plan);
-            if(A == 0){
-              move_group.move();
-              A = 1;
-            }
-            else if(A == 1){
-              trajectory.joint_trajectory.joint_names = my_plan.trajectory_.joint_trajectory.joint_names;
-              joint_model_group = start_state->getJointModelGroup(move_group.getName());
-              start_state->setJointGroupPositions(joint_model_group, target_joints);
-              move_group.setStartState(*start_state);
-              for(int j = 0; j < my_plan.trajectory_.joint_trajectory.points.size(); j++){
-                trajectory.joint_trajectory.points.push_back(my_plan.trajectory_.joint_trajectory.points[j]);
-              }
-            }
-            //move_group.setMaxVelocityScalingFactor(1);
-            //move_group.setMaxAccelerationScalingFactor(1);
-            //size_t colon_pos_E = file_line.find('E');
-            //if(colon_pos_E < 100){
-            //  if(stod(file_line.substr(colon_pos_E+1)) > 0){
-            //    visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
-            //  }
-            //}
-            //visual_tools.trigger();
-          }
-        }
-        if(K == 1){
-          moveit::planning_interface::MoveGroupInterface::Plan joinedPlan;
-          robot_trajectory::RobotTrajectory rt(move_group.getCurrentState()->getRobotModel(), "arm");
-          rt.setRobotTrajectoryMsg(*move_group.getCurrentState(),trajectory);
-          trajectory_processing::IterativeParabolicTimeParameterization iptp;
-          iptp.computeTimeStamps(rt);
-          rt.getRobotTrajectoryMsg(trajectory);
-          joinedPlan.trajectory_ = trajectory;
-          if(!move_group.execute(joinedPlan)){
-            ROS_ERROR("Failed to execute plan");
-            return false;
-          }
-          end_ = ros::WallTime::now();
-          double execution_time = (end_ - start_).toNSec() * 1e-9;
-          ROS_INFO_STREAM("Exectution time (ms): " << execution_time);
-          ros::shutdown();
-          return 0;
+        else{
+          moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+          move_group.setPlanningTime(0.05);
+          move_group.plan(my_plan);
+          move_group.move();
         }
       }
     }
   }
+  moveit::planning_interface::MoveGroupInterface::Plan joinedPlan;
+  robot_trajectory::RobotTrajectory rt(move_group.getCurrentState()->getRobotModel(), "arm");
+  rt.setRobotTrajectoryMsg(*move_group.getCurrentState(),trajectory);
+  trajectory_processing::IterativeParabolicTimeParameterization iptp;
+  iptp.computeTimeStamps(rt);
+  rt.getRobotTrajectoryMsg(trajectory);
+  std::cout << trajectory;
+  joinedPlan.trajectory_ = trajectory;
+  if(!move_group.execute(joinedPlan)){
+    ROS_ERROR("Failed to execute plan");
+    return false;
+  }
+  end_ = ros::WallTime::now();
+  double execution_time = (end_ - start_).toNSec() * 1e-9;
+  ROS_INFO_STREAM("Exectution time (ms): " << execution_time);
+  ros::shutdown();
+  return 0;
 }
